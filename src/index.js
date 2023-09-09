94c47edc84db5bb1ecb710e59439740396db0432
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-09-09 15:15:42 -0700

    Create Redux store and implement by wrapping App in Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { legacy_createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducer from './reducers';
@@ -9,9 +9,17 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const rootReducer = combineReducers({
+  componentState: reducer
+})
+
+const store = legacy_createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  <Router>
+  <Provider store={store} >
+    <Router>
       <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
