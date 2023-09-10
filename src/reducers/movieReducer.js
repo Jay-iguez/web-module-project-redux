1055f6223ce45559aa65318de8bc6d2fa6c45e24
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-09-09 22:02:07 -0700

    Create logic to delete and add a movie in AddMovieForm and Movie

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,7 +10,22 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
+            }
+        case ADD_MOVIE:
+            const newMovie = {
+                id: state.movies.length,
+                title: action.payload.title,
+                director: action.payload.director,
+                metascore: action.payload.metascore,
+                genre: action.payload.genre,
+                description: action.payload.description
+            }
+            console.log(newMovie)
+            return {
+                ...state,
+                movies: [...state.movies, newMovie]
             }
         default:
             return state;
